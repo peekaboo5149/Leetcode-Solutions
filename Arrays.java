54966089be1902113200207ee769c01fc4cff682
peekaboo <ktanmay5149@gmail.com>
2021-03-04 13:46:28 +0530

    More problems

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Arrays {
     public int[] runningSum(int[] nums) {
         int[] res = new int[nums.length];
@@ -8,4 +10,24 @@ public class Arrays {
         }
         return res;
     }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int max = candies[0];
+
+        for (int i = 0; i < candies.length; i++)
+            if (candies[i] > max)
+                max = candies[i];
+
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max)
+                ans.add(true);
+            else
+                ans.add(false);
+        }
+
+        return ans;
+    }
+
+   
 }
