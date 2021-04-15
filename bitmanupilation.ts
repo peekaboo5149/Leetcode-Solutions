45930934ac1682a11872a684653dacd9d536d7c6
peekaboo <ktanmay5149@gmail.com>
2021-04-15 06:40:50 +0530

    Solved2

--- bitmanupilation.ts
+++ bitmanupilation.ts
@@ -15,3 +15,31 @@ function xorOperation(n: number, start: number): number {
   for (let i = 0; i < n; i++) res ^= start + (i << 1);
   return res;
 }
+
+class _ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function getDecimalValue(head: _ListNode | null): number {
+  let pow = 0,
+    res = 0;
+  let temp = head;
+  while (temp != null) {
+    pow++;
+    temp = temp.next;
+  }
+
+  temp = head;
+  while (temp != null) {
+    res += temp.val * Math.pow(2, pow - 1);
+    temp = temp.next;
+    pow--;
+  }
+
+  return res;
+}
